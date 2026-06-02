c3232ec1b810085c5b2b6c1a57921673f15b24d7
ing-ganaha <ganaha@in-g.jp>
2026-06-02 14:53:57 +0900

    chore: reactを追加

---
+++ resources/ts/router.tsx
@@ -0,0 +1,9 @@
+import { createBrowserRouter } from 'react-router'
+import { Button } from '@chakra-ui/react'
+
+export const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Button colorPalette="teal">Hello React Router</Button>,
+    },
+])
